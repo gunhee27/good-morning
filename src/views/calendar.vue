8fbbe47a3df339c82db3f46bbb5e8dbbf9b01b02
geonhee <gunhee27@naver.com>
2024-02-04 23:57:00 +0900

    [STYLE] 캘린더 기본 레이아웃 구성

--- src/views/calendar.vue
+++ src/views/calendar.vue
@@ -1,12 +1,37 @@
 <template>
-    <div>
-      <VCalendar expanded/>
+    <div class="layout">
+      <DatePicker v-model.string="date" :masks="masks" expanded/>
+      <div class="today">
+        <p>날짜 : {{ date }}</p>
+        <p>날씨 : </p>
+        <p>할일 : <textarea /></p>
+        <p>뉴스 : <textarea /></p>
+      </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import 'v-calendar/style.css';
+import { DatePicker } from 'v-calendar';
+import { ref } from 'vue';
+
+const date = ref(new Date().toISOString().split('T')[0]);
+
+const masks = ref({
+  modelValue: 'YYYY-MM-DD',
+}); 
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.today{
+  height: 200px;
+  border: 1px solid;
+  border-radius: 20px;
+  width: 50vw;
+  p{
+    width: fit-content;
+    margin: 10px; 
+  }
+  
+}
 </style>
